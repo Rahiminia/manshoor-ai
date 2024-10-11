7b32de5eaea8c8f376c796213020b3c24c35d25e
Hassan Rahimi <rahiminia.ai@gmail.com>
2024-10-11 14:26:49 +0330

    - Adds static files & index.html serve functionality to flask - Adds http response codes to api

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from generate import generate
 
 app = Flask(__name__)
 
+@app.route('/')
+def root():
+    print('root')
+    return render_template('index.html'), 200
+
 @app.get('/api')
 def test():
-    return "SohrabAI"
+    return "SohrabAI", 200
 
 @app.get('/api/generate')
 def generate_verse():
     verse = request.args.get('verse')
     if not verse or len(verse) < 0:
-        return jsonify(message="Please provide a sample verse")
+        return jsonify(message="Please provide a sample verse"), 400
     output = generate(verse)
-    return jsonify(verse=output)
+    return jsonify(verse=output), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000)
